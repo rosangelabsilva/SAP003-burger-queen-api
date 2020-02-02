446c51c786df69c08ca864fd6528e84cf2c30096
rosangelabsilva <rosangela-bsilva@hotmail.com>
2020-02-02 18:12:14 -0300

    test

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const minha_porta = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT);
+app.listen(minha_porta);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
