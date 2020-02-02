4938d21f5b22e40427c4f38b5d63f9c5d64e4960
rosangelabsilva <rosangela-bsilva@hotmail.com>
2020-02-02 18:20:43 -0300

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
