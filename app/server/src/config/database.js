2f242a1ab068e480e809688a3c5844c2074da6c1
rosangelabsilva <rosangela-bsilva@hotmail.com>
2020-02-02 17:43:21 -0300

    setting up environment

--- app/server/src/config/database.js
+++ app/server/src/config/database.js
@@ -1,8 +1,31 @@
+require('dotenv').config();
+
 module.exports = {
-  username: 'postgres',
-  password: 'root',
-  database: 'burguerqueen',
-  host: 'db',
-  dialect: 'postgres',
-  operatorsAliases: false
+  "development": {
+    "username": 'postgres',
+    "password": null,
+    "database": 'burguerqueen',
+    "host": 'bq-api-node_db',
+    "dialect": 'postgres',
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "burguerqueen_test",
+    "host": "bq-api-node_db",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "operatorsAliases": false,
+    "dialectOptions": {
+      "ssl": true
+    }
+  }
 }
